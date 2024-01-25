7fa1f5e081fac3c91fbb483fe73ef711c2fc5ec4
Viviane Le <anhlv.fpt@gmail.com>
2024-01-25 12:31:02 +0100

    lab03 - prepare functions, add tasks & flows

--- lessons/03-pipeline-and-orchestration/lib/modeling.py
+++ lessons/03-pipeline-and-orchestration/lib/modeling.py
@@ -1,1 +1,18 @@
-# To complete
+# To compleimport os
+import numpy as np
+import scipy.sparse
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+
+def train_model(X: scipy.sparse.csr_matrix, y: np.ndarray) -> LinearRegression:
+    lr = LinearRegression()
+    lr.fit(X, y)
+    return lr
+
+def predict(X: scipy.sparse.csr_matrix, model: LinearRegression) -> np.ndarray:
+    return model.predict(X)
+
+
+def evaluate_model(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    return mean_squared_error(y_true, y_pred, squared=False)
